ff3a170fe39eb665a04c7084eab41639717e4080
aquafinawater <jamiegao@ufl.edu>
2017-09-26 13:46:57 -0400

    My code states a mathematical contradiction due to the nature of coding.

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Lab 4 Github upload
+
+        System.out.println("This is a test program to see if we can upload this to github.");
+
+        int x = 5;
+        int y = 3;
+        int z = 1;
+
+        System.out.println("Since 1*3=" + y*z + " and ((5/3)*3=" + (x/y)*y + ", then 1=(5/3).");
+    }
+}
